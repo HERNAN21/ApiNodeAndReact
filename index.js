9cab12b8e6513c3c00f6139cb8cc9b0f16976612
Hernan <hernanrojasutani@gmail.com>
2019-10-09 15:51:13 -0500

    -Config server esxpress

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+var cors = require("cors")
+var bodyParser = require("body-parser")
+
+var port = process.env.PORT || 6000
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+var API = require('./server/routes/API')
+
+app.use(API)
+app.listen(port, () => {
+    console.log("Server running on port: " + port)
+})
+
+
---
+++ server/routes/API.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const api = express.Router()
+const cors = require('cors')
+// const db = require("../database/db_local")
+api.use(cors())
+// process.env.SECRET_KEY = 'secret'
+const api_name = "/pdr_api/v1";
+
+// const AWS = require('aws-sdk');
+// global.fetch = require('node-fetch');
+
+// Start api solicitud
+// Parameters
+api.get(api_name + '/test', (req, res) => {
+    // console.log('Hola Mundo')
+    res.json({'result':'success','mesage':'hello world'});
+    // db.sequelize
+    //     .query('select codigo as value, descripcion as label,* from general where grupo = :grupo',
+    //         { replacements: { grupo: req.params.grupo }, type: db.sequelize.QueryTypes.SELECT }
+    //     )
+    //     .then((result) => {
+    //         res.json(result);
+    //     });
+});
+
+
+module.exports = api
